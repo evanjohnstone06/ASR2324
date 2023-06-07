1347ab8279248bc149e75a27a40b49a9ab1b8149
evan johnstone <44927564+evanjohnstone06@users.noreply.github.com>
2023-06-07 16:46:21 +0100

    Create lidarRot.py
    File which will rotate our servo as well as gather data from LiDAR sensor

---
+++ lidarRot.py
@@ -0,0 +1,152 @@
+import RPi.GPIO as GPIO
+import os
+import socket
+import fcntl
+import struct
+import time
+import serial
+from time import gmtime, strftime, sleep
+from gpiozero import Button
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Servo
+import servoThing
+
+ser = serial.Serial("/dev/ttyAMA0", 115200)
+
+# Define GPIO to LCD mapping
+LCD_RS = 26
+LCD_E  = 19
+LCD_D4 = 13
+LCD_D5 = 6
+LCD_D6 = 5
+LCD_D7 = 11
+
+
+# Define some device constants
+LCD_WIDTH = 16    # Maximum characters per line
+LCD_CHR = True
+LCD_CMD = False
+
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+# Main program block
+  
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
+GPIO.setup(LCD_E, GPIO.OUT)  # E
+GPIO.setup(LCD_RS, GPIO.OUT) # RS
+GPIO.setup(LCD_D4, GPIO.OUT) # DB4
+GPIO.setup(LCD_D5, GPIO.OUT) # DB5
+GPIO.setup(LCD_D6, GPIO.OUT) # DB6
+GPIO.setup(LCD_D7, GPIO.OUT) # DB7
+
+def lcd_toggle_enable():
+  # Toggle enable
+  time.sleep(E_DELAY)
+  GPIO.output(LCD_E, True)
+  time.sleep(E_PULSE)
+  GPIO.output(LCD_E, False)
+  time.sleep(E_DELAY)
+  
+def lcd_byte(bits, mode):
+  # Send byte to data pins
+  # bits = data
+  # mode = True  for character
+  #        False for command
+
+  GPIO.output(LCD_RS, mode) # RS
+
+  # High bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x10==0x10:
+    GPIO.output(LCD_D4, True)
+  if bits&0x20==0x20:
+    GPIO.output(LCD_D5, True)
+  if bits&0x40==0x40:
+    GPIO.output(LCD_D6, True)
+  if bits&0x80==0x80:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+
+  # Low bits
+  GPIO.output(LCD_D4, False)
+  GPIO.output(LCD_D5, False)
+  GPIO.output(LCD_D6, False)
+  GPIO.output(LCD_D7, False)
+  if bits&0x01==0x01:
+    GPIO.output(LCD_D4, True)
+  if bits&0x02==0x02:
+    GPIO.output(LCD_D5, True)
+  if bits&0x04==0x04:
+    GPIO.output(LCD_D6, True)
+  if bits&0x08==0x08:
+    GPIO.output(LCD_D7, True)
+
+  # Toggle 'Enable' pin
+  lcd_toggle_enable()
+
+def lcd_init():
+  # Initialise display
+  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number  of lines, font size
+  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+  time.sleep(E_DELAY)
+
+# Initialise display
+lcd_init()
+
+def lcd_string(message,line):
+  # Cast to string
+  message = str(message)
+  # Send string to display
+  message = message.ljust(LCD_WIDTH," ")
+
+  lcd_byte(line, LCD_CMD)
+
+  for i in range(LCD_WIDTH):
+    lcd_byte(ord(message[i]),LCD_CHR)
+
+def getTFminiData():
+    time.sleep(0.2)
+    count = ser.in_waiting
+    if count > 8:
+        recv = ser.read(9)   
+        ser.reset_input_buffer() 
+            # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
+            # type(recv[0]), 'str' in python2, 'int' in python3 
+        if recv[0] == 0x59 and recv[1] == 0x59:     #python3
+            distance = recv[2] + recv[3] * 256
+            strength = recv[4] + recv[5] * 256
+            lcd_string(str(distance),LCD_LINE_1)
+            ser.reset_input_buffer()
+while True:
+	getTFminiData()
+	servoThing.servoSweep()
+	
+
+
+
+if __name__ == '__main__':
+    try:
+        if ser.is_open == False:
+            ser.open()
+        getTFminiData()
+    except KeyboardInterrupt:   # Ctrl+C
+        if ser != None:
+            ser.close()
+                
+
+
