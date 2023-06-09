676a675527fd0f07ea9b1d85d2b0aebf0ad62ece
evan johnstone <44927564+evanjohnstone06@users.noreply.github.com>
2023-06-09 14:46:52 -0500

    Update lidarRot.py
    Servo will now stop when LiDAR reads a distance that we deem "too close" (20cm for testing)

--- lidarRot.py
+++ lidarRot.py
@@ -11,120 +11,26 @@ from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import Servo
 import servoThing
 from multiprocessing import Process
+from multiprocessing import Pool
 import sys
+from signal import signal, SIGTERM, SIGHUP, pause
+from rpi_lcd import LCD
+import math
+
 
 rocket = 0
-
+stopS = False
+factory = PiGPIOFactory()
+serv = Servo(18, pin_factory = factory, min_pulse_width = 0.5/1000, max_pulse_width = 1.5/1000)
 ser = serial.Serial("/dev/ttyAMA0", 115200)
 
-# Define GPIO to LCD mapping
-LCD_RS = 26
-LCD_E  = 19
-LCD_D4 = 13
-LCD_D5 = 6
-LCD_D6 = 5
-LCD_D7 = 11
+lcd = LCD()
 
-
-# Define some device constants
-LCD_WIDTH = 16    # Maximum characters per line
-LCD_CHR = True
-LCD_CMD = False
-
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
-
-# Timing constants
-E_PULSE = 0.0005
-E_DELAY = 0.0005
-
-# Main program block
-  
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
-GPIO.setup(LCD_E, GPIO.OUT)  # E
-GPIO.setup(LCD_RS, GPIO.OUT) # RS
-GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-GPIO.setup(LCD_D7, GPIO.OUT) # DB7
-
-def lcd_toggle_enable():
-  # Toggle enable
-  time.sleep(E_DELAY)
-  GPIO.output(LCD_E, True)
-  time.sleep(E_PULSE)
-  GPIO.output(LCD_E, False)
-  time.sleep(E_DELAY)
-  
-def lcd_byte(bits, mode):
-  # Send byte to data pins
-  # bits = data
-  # mode = True  for character
-  #        False for command
-
-  GPIO.output(LCD_RS, mode) # RS
-
-  # High bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x10==0x10:
-    GPIO.output(LCD_D4, True)
-  if bits&0x20==0x20:
-    GPIO.output(LCD_D5, True)
-  if bits&0x40==0x40:
-    GPIO.output(LCD_D6, True)
-  if bits&0x80==0x80:
-    GPIO.output(LCD_D7, True)
-
-  # Toggle 'Enable' pin
-  lcd_toggle_enable()
-
-  # Low bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x01==0x01:
-    GPIO.output(LCD_D4, True)
-  if bits&0x02==0x02:
-    GPIO.output(LCD_D5, True)
-  if bits&0x04==0x04:
-    GPIO.output(LCD_D6, True)
-  if bits&0x08==0x08:
-    GPIO.output(LCD_D7, True)
-
-  # Toggle 'Enable' pin
-  lcd_toggle_enable()
-
-def lcd_init():
-  # Initialise display
-  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
-  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number  of lines, font size
-  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
-  time.sleep(E_DELAY)
-
-# Initialise display
-lcd_init()
-
-def lcd_string(message,line):
-  # Cast to string
-  message = str(message)
-  # Send string to display
-  message = message.ljust(LCD_WIDTH," ")
-
-  lcd_byte(line, LCD_CMD)
-
-  for i in range(LCD_WIDTH):
-    lcd_byte(ord(message[i]),LCD_CHR)
+    
 def getTFminiData():
     while True:
         global rocket
+        tooClose = False
         time.sleep(0.2)
         count = ser.in_waiting
         if count > 8:
@@ -134,24 +40,66 @@ def getTFminiData():
             # type(recv[0]), 'str' in python2, 'int' in python3 
             if recv[0] == 0x59 and recv[1] == 0x59:     #python3
                 distance = recv[2] + recv[3] * 256
+                return distance
                 strength = recv[4] + recv[5] * 256
-                lcd_string(str(distance),LCD_LINE_1)
+                lcd.text(str(distance), 1)
                 print(distance)
                 ser.reset_input_buffer()
+                #return distance
+                if distance <= 20:
+                    tooClose = True
+                    lcd.text(str(tooClose), 2)
+                    
+                else:
+                    tooClose = False
+                    lcd.text(str(tooClose), 2)
         while rocket < 999999:
             rocket += 1
 
+def servoSweep():
+    servStage = 1
+    while True:
+        
+        dist = getTFminiData()
+        global rocket
+        if dist < 20:
+            #serv.detach()
+            print('u should stawp')
+        else:
+            #servStage = 4
+            print('servVal', serv.value)
+            print('stage: ', servStage)
+            if servStage==1:
+                serv.value+=0.1
+                #sleep(0.01)
+                if serv.value>=0.9:
+                    servStage=2
+            if servStage==2:
+                serv.value-=0.1
+                #sleep(0.01)
+                if serv.value<=-0.9:
+                    servStage=1
+                
+        print(dist)
+
+        
+    while rocket < sys.maxint:
+        rocket += 1
+
+
+
+pool = Pool()
 if __name__ == '__main__':
     try:
+        
         if ser.is_open == False:
             ser.open()
         p1 = Process(target = getTFminiData)
         p1.start()
-        p2 = Process(target = servoThing.servoSweep)
+        p2 = Process(target = servoSweep)
         p2.start()
+
     except KeyboardInterrupt:   # Ctrl+C
         if ser != None:
             ser.close()
                 
-
-
