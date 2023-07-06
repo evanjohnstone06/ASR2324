d540fbadd03258ed661a543b410ec5804096b751
evan johnstone <44927564+evanjohnstone06@users.noreply.github.com>
2023-07-06 17:44:55 +0100

    Create cameraLidar.py

---
+++ system_code/cameraLidar.py
@@ -0,0 +1,387 @@
+# Import packages
+import os
+import argparse
+import cv2
+import numpy as np
+import sys
+import time
+from threading import Thread
+import importlib.util
+import RPi.GPIO as GPIO
+import datetime
+import socket
+import fcntl
+import struct
+import serial
+from time import gmtime, strftime, sleep
+from gpiozero import Button
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Servo
+import servoThing
+from multiprocessing import Process
+from multiprocessing.shared_memory import SharedMemory
+from multiprocessing import Pool
+import sys
+from signal import signal, SIGTERM, SIGHUP, pause
+import math
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(10, GPIO.OUT)
+GPIO.setup(9, GPIO.OUT)
+rocket = 0
+stopS = False
+factory = PiGPIOFactory()
+serv = Servo(18, pin_factory = factory, min_pulse_width = 0.5/1000, max_pulse_width = 1.5/1000)
+ser = serial.Serial("/dev/ttyAMA0", 115200)
+
+def getTFminiData():
+    while True:
+        global rocket
+        time.sleep(0.2)
+        count = ser.in_waiting
+        if count > 8:
+            recv = ser.read(9)   
+            ser.reset_input_buffer()
+            # type(recv), 'str' in python2(recv[0] = 'Y'), 'bytes' in python3(recv[0] = 89)
+            # type(recv[0]), 'str' in python2, 'int' in python3 
+            if recv[0] == 0x59 and recv[1] == 0x59:     #python3
+                distance = recv[2] + recv[3] * 256
+                print(distance)
+                ser.reset_input_buffer()
+                #return distance
+                if distance <= 200:
+                    #when share.buf[0] is set to 1, it tells the servo to stop
+                    share.buf[0]=1
+                else:
+                    share.buf[0]=0
+        while rocket < 999999:
+            rocket += 1
+
+def servoSweep():
+    servStage = 1
+    #i = 0
+    while True: 
+        global rocket
+        
+        #i += 5
+        global rocket
+        for i in range(0, 360):
+            sleep(0.005)
+            while share.buf[0]==1:
+                print('stop servo')
+            serv.value = math.sin(math.radians(i))
+            sleep(0.003)
+        
+    while rocket < sys.maxint:
+        rocket += 1
+pool = Pool()
+
+# Define VideoStream class to handle streaming of video from webcam in separate processing thread
+# Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
+def runCamera():
+    class VideoStream:
+        """Camera object that controls video streaming from the Picamera"""
+        def __init__(self,resolution=(640,480),framerate=30):
+            # Initialize the PiCamera and the camera image stream
+            self.stream = cv2.VideoCapture(0)
+            ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+            ret = self.stream.set(3,resolution[0])
+            ret = self.stream.set(4,resolution[1])
+                
+            # Read first frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+    
+        # Variable to control when the camera is stopped
+            self.stopped = False
+    
+        def start(self):
+        # Start the thread that reads frames from the video stream
+            Thread(target=self.update,args=()).start()
+            return self
+    
+        def update(self):
+            # Keep looping indefinitely until the thread is stopped
+            while True:
+                # If the camera is stopped, stop the thread
+                if self.stopped:
+                    # Close camera resources
+                    self.stream.release()
+                    return
+    
+                # Otherwise, grab the next frame from the stream
+                (self.grabbed, self.frame) = self.stream.read()
+    
+        def read(self):
+        # Return the most recent frame
+            return self.frame
+    
+        def stop(self):
+        # Indicate that the camera and thread should be stopped
+            self.stopped = True
+    
+    # Define and parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--modeldir', help='Folder the .tflite file is located in',
+                        required=True)
+    parser.add_argument('--graph', help='Name of the .tflite file, if different than detect.tflite',
+                        default='detect.tflite')
+    parser.add_argument('--labels', help='Name of the labelmap file, if different than labelmap.txt',
+                        default='labelmap.txt')
+    parser.add_argument('--threshold', help='Minimum confidence threshold for displaying detected objects',
+                        default=0.5)
+    parser.add_argument('--resolution', help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.',
+                        default='1280x720')
+    parser.add_argument('--edgetpu', help='Use Coral Edge TPU Accelerator to speed up detection',
+                        action='store_true')
+    
+    dangerZoneTL = (int(0* 1280),int(0)) #0*1280, 720*0.3
+    dangerZoneBR = (int(1280),int(720)) #1280*0.3, 720
+    
+    inDangerZone = False
+    dangerZoneCounter = 0
+    last = datetime.datetime.now()
+    args = parser.parse_args()
+    
+    MODEL_NAME = args.modeldir
+    GRAPH_NAME = args.graph
+    LABELMAP_NAME = args.labels
+    min_conf_threshold = float(args.threshold)
+    resW, resH = args.resolution.split('x')
+    imW, imH = int(resW), int(resH)
+    use_TPU = args.edgetpu
+    
+    cars = 0
+    
+    # Import TensorFlow libraries
+    # If tflite_runtime is installed, import interpreter from tflite_runtime, else import from regular tensorflow
+    # If using Coral Edge TPU, import the load_delegate library
+    pkg = importlib.util.find_spec('tflite_runtime')
+    if pkg:
+        from tflite_runtime.interpreter import Interpreter
+        if use_TPU:
+            from tflite_runtime.interpreter import load_delegate
+    else:
+        from tensorflow.lite.python.interpreter import Interpreter
+        if use_TPU:
+            from tensorflow.lite.python.interpreter import load_delegate
+    
+    # If using Edge TPU, assign filename for Edge TPU model
+    if use_TPU:
+        # If user has specified the name of the .tflite file, use that name, otherwise use default 'edgetpu.tflite'
+        if (GRAPH_NAME == 'detect.tflite'):
+            GRAPH_NAME = 'edgetpu.tflite'       
+    
+    # Get path to current working directory
+    CWD_PATH = os.getcwd()
+    
+    # Path to .tflite file, which contains the model that is used for object detection
+    PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
+    
+    # Path to label map file
+    PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
+    
+    # Load the label map
+    with open(PATH_TO_LABELS, 'r') as f:
+        labels = [line.strip() for line in f.readlines()]
+    
+    # Have to do a weird fix for label map if using the COCO "starter model" from
+    # https://www.tensorflow.org/lite/models/object_detection/overview
+    # First label is '???', which has to be removed.
+    if labels[0] == '???':
+        del(labels[0])
+    
+    # Load the Tensorflow Lite model.
+    # If using Edge TPU, use special load_delegate argument
+    if use_TPU:
+        interpreter = Interpreter(model_path=PATH_TO_CKPT,
+                                  experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
+        print(PATH_TO_CKPT)
+    else:
+        interpreter = Interpreter(model_path=PATH_TO_CKPT)
+    
+    interpreter.allocate_tensors()
+    
+    # Get model details
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    height = input_details[0]['shape'][1]
+    width = input_details[0]['shape'][2]
+    
+    #custom variables
+    counterValue = 0
+    x = 0
+    y = 0
+    floating_model = (input_details[0]['dtype'] == np.float32)
+    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(11,GPIO.OUT)
+    
+    input_mean = 127.5
+    input_std = 127.5
+    
+    # Check output layer name to determine if this model was created with TF2 or TF1,
+    # because outputs are ordered differently for TF2 and TF1 models
+    outname = output_details[0]['name']
+    
+    if ('StatefulPartitionedCall' in outname): # This is a TF2 model
+        boxes_idx, classes_idx, scores_idx = 1, 3, 0
+    else: # This is a TF1 model
+        boxes_idx, classes_idx, scores_idx = 0, 1, 2
+    
+    # Initialize frame rate calculation
+    frame_rate_calc = 1
+    freq = cv2.getTickFrequency()
+    
+    # Initialize video stream
+    videostream = VideoStream(resolution=(imW,imH),framerate=30).start()
+    time.sleep(1)
+    
+    #for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+    while True:
+    
+        # Start timer (for calculating frame rate)
+        t1 = cv2.getTickCount()
+    
+        # Grab frame from video stream
+        frame1 = videostream.read()
+    
+        # Acquire frame and resize to expected shape [1xHxWx3]
+        frame = frame1.copy()
+        frame_f = cv2.flip(frame, 0)
+        frame_rgb = cv2.cvtColor(frame_f, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb, (width, height))
+        input_data = np.expand_dims(frame_resized, axis=0)
+        
+    
+        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+        if floating_model:
+            input_data = (np.float32(input_data) - input_mean) / input_std
+    
+        # Perform the actual detection by running the model with the image as input
+        interpreter.set_tensor(input_details[0]['index'],input_data)
+        interpreter.invoke()
+    
+        # Retrieve detection results
+        boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
+        classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
+        scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
+    
+        # Loop over all detections and draw detection box if confidence is above minimum threshold
+        for i in range(len(scores)):
+            if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+    
+                # Get bounding box coordinates and draw box
+                # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+                ymin = int(max(1,(boxes[i][0] * imH)))
+                xmin = int(max(1,(boxes[i][1] * imW)))
+                ymax = int(min(imH,(boxes[i][2] * imH)))
+                xmax = int(min(imW,(boxes[i][3] * imW)))
+                
+    
+                
+                
+                cv2.rectangle(frame_f, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+    
+                # Draw label
+                object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+                label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+                label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+                cv2.rectangle(frame_f, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+                cv2.putText(frame_f, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+                
+                
+                if object_name == 'person':
+                    x = int(((boxes[0][1]+boxes[0][3])/2)*1280)
+                    y = int(((boxes[0][0]+boxes[0][2])/2)*720)
+                    #print('detected')
+                    #GPIO.output(11,GPIO.HIGH)
+                    if ((object_name == 'person') and ((x > dangerZoneTL[0]) and (x < dangerZoneBR[0]) and (y > dangerZoneTL[1]) and (y < dangerZoneBR[1]))):
+                        dangerZoneCounter = 1
+                        counterValue = 1
+                        inDangerZone = True
+                        share.buf[1]=1
+                        #print('howdy')
+                       
+    
+                        
+    
+                    else:
+                        counterValue = 0
+                        last = datetime.datetime.now()
+                        dangerZoneCounter = 0
+                        inDangerZone = False
+                        share.buf[1]=0
+                else:
+                    counterValue = 0
+                    share.buf[1]=0
+                    #last = datetime.datetime.now()
+                
+                cars += counterValue
+            
+        cv2.rectangle(frame_f, dangerZoneTL, dangerZoneBR, (255, 20, 20), 3)
+        cv2.putText(frame_f, "Danger Zone", (dangerZoneTL[0]+10, dangerZoneBR[0]-10), cv2.FONT_HERSHEY_PLAIN, 2, (255, 20, 255), 3, cv2.LINE_AA)		
+        cv2.putText(frame_f,'Total cars:',(20,80),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),4,cv2.LINE_AA)
+        cv2.putText(frame_f,'Total cars:',(20,80),cv2.FONT_HERSHEY_PLAIN,2,(230,230,230),2,cv2.LINE_AA)
+        cv2.putText(frame_f,'%.2f' % cars,(260,85),cv2.FONT_HERSHEY_PLAIN,2.5,(0,0,0),4,cv2.LINE_AA)
+        cv2.putText(frame_f,'%.2f' % cars,(260,85),cv2.FONT_HERSHEY_PLAIN,2.5,(85,195,105),2,cv2.LINE_AA)
+        cv2.putText(frame_f, str(x), (200, 50), cv2.FONT_HERSHEY_PLAIN, 2.5, (85, 100, 100), 2, cv2.LINE_AA)
+        cv2.putText(frame_f, str(y), (200, 100), cv2.FONT_HERSHEY_PLAIN, 2.5, (85, 100, 100), 2, cv2.LINE_AA)
+    
+    
+        # Draw framerate in corner of frame
+        cv2.putText(frame_f,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
+    
+        # All the results have been drawn on the frame, so it's time to display it.
+        #cv2.imshow('Object detector', frame_f)
+    
+        # Calculate framerate
+        t2 = cv2.getTickCount()
+        time1 = (t2-t1)/freq
+        frame_rate_calc= 1/time1
+        
+        cars = 0
+        # Press 'q' to quit
+        if cv2.waitKey(1) == ord('q'):
+            break
+def turnOnTheLights():
+    last = datetime.datetime.now()
+    while True:
+        if (share.buf[1]==1) and (share.buf[0] == 1):
+            GPIO.output(11, GPIO.HIGH)
+            GPIO.output(9, GPIO.LOW)
+            GPIO.output(10, GPIO.LOW)
+            last = datetime.datetime.now()
+        elif (share.buf[1]==1) or (share.buf[0]==1):
+            last =  datetime.datetime.now()
+            GPIO.output(9,GPIO.HIGH)
+            GPIO.output(10, GPIO.LOW)
+            GPIO.output(11, GPIO.LOW)
+        else:
+            if datetime.datetime.now() - last > datetime.timedelta(seconds = 1):
+                GPIO.output(10,GPIO.HIGH)
+                GPIO.output(11,GPIO.LOW)
+                GPIO.output(9, GPIO.LOW)
+                print('nothing')
+if __name__ == '__main__':
+    try:
+        share=SharedMemory(create=True,size=1024)
+        if ser.is_open == False:
+            ser.open()
+        p1 = Process(target = getTFminiData)
+        p1.start()
+        p2 = Process(target = servoSweep)
+        p2.start()
+        p3 = Process(target=runCamera)
+        p3.start()
+        p4 = Process(target=turnOnTheLights)
+        p4.start()
+    except KeyboardInterrupt:   # Ctrl+C
+        if ser != None:
+            ser.close()
+# Clean up
+cv2.destroyAllWindows()
+#videostream.stop()
